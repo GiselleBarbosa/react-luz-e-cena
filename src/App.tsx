74b89c76aad2931892670c713a4dce9b24469af2
GiselleBarbosa <g88barbosa@gmail.com>
2026-01-04 16:40:08 -0300

    feat: finalizando componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import Banner from './Components/Banner';
-import Link from './Components/Link';
+import Header from './Components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Banner src="../public/images/Banners/banner.png" alt="Banner" />
-      <Link href="/" target="_blank">
-        {' '}
-        Link
-      </Link>
     </>
   );
 }
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,5 +1,32 @@
+import Logo from '../Logo';
+import HeaderActions from './components/HeaderActions';
+import HeaderFormFilters from './components/HeaderFormFilters';
+import HeaderLinks from './components/HeaderLinks';
+import HeaderList from './components/HeaderList';
+import HeaderListItem from './components/HeaderListItem';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <HeaderList>
+        <HeaderListItem>
+          <Logo src="../public/icons/Logos/Logo-2.png" />
+        </HeaderListItem>
+
+        <HeaderListItem>
+          <HeaderLinks />
+        </HeaderListItem>
+
+        <HeaderListItem>
+          <HeaderFormFilters></HeaderFormFilters>
+        </HeaderListItem>
+
+        <HeaderListItem>
+          <HeaderActions></HeaderActions>
+        </HeaderListItem>
+      </HeaderList>
+    </header>
+  );
 };
 
 export default Header;
