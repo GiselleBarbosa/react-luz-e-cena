8cd2079741f7e845853949703a41665456f20696
GiselleBarbosa <g88barbosa@gmail.com>
2026-01-04 13:35:55 -0300

    feat: adiciona componente Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import Banner from "./Components";
+import Banner from "./Components/Banner";
+import Link from "./Components/Links";
 
 function App() {
-	return <Banner src="../public/images/Banners/banner.png" alt="Banner" />;
+	return (
+		<>
+			<Banner src="../public/images/Banners/banner.png" alt="Banner" />
+			<Link href="/" target='_blank'> Link</Link>
+		</>
+	);
 }
 
 export default App;
