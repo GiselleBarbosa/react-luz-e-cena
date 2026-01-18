bd56e9418fc954877a6cf242a97fb9ffc398c3dd
GiselleBarbosa <g88barbosa@gmail.com>
2026-01-18 15:03:03 -0300

    feat: implementando hook para filtro para busca de filmes

--- src/Components/MovieSection/index.tsx
+++ src/Components/MovieSection/index.tsx
@@ -5,16 +5,24 @@ import InputText from '../InputText';
 import MovieList from '../MovieList';
 import styles from './MovieSection.module.css';
 import useFetchMovies from '../../hooks/useFetchMovies';
+import useFilterMovies from '../../hooks/useFilterMovies';
 
 const MovieSection = () => {
   const { movies, isLoading, error } = useFetchMovies();
+  const { searchTerm, handleSearch, setSearchTerm, filteredMovies } = useFilterMovies(movies);
 
   return (
     <main>
       <section className={styles.container}>
         <Fieldset variant="secondary">
-          <InputText placeholder="Buscar filmes..." />
-          <Button variant="icon">
+          <InputText
+            value={searchTerm}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setSearchTerm(event?.target.value)
+            }
+            placeholder="Buscar filmes..."
+          />
+          <Button variant="icon" onClick={handleSearch}>
             <FaSearch />
           </Button>
         </Fieldset>
@@ -22,8 +30,8 @@ const MovieSection = () => {
         <h1 className={styles.titulo}>Em cartaz</h1>
 
         {isLoading && <p>Carregando filmes...</p>}
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        {!isLoading && !error && <MovieList movies={movies} />}
+        {error && <p className={styles.error}>{error}</p>}
+        {!isLoading && !error && <MovieList movies={filteredMovies} />}
       </section>
     </main>
   );
