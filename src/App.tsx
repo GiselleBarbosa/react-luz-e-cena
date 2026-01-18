6d3195f521e536ed2ec5e8178eae0bd5a931aa34
GiselleBarbosa <g88barbosa@gmail.com>
2026-01-18 15:22:05 -0300

    feat: adiciona newsletter e banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Banner from './components/Banner';
 import Header from './components/Header';
 import MovieSection from './components/MovieSection';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Header />
       <Banner src="../images/Banners/banner.png" alt="Banner" />
       <MovieSection />
+      <Banner src="../images/Banners/combo.png" alt="Combo" />
+      <Newsletter />
     </>
   );
 }
